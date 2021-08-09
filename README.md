af59ad4691ffe55814c74144b8e162c8884bcbc8
casacovermelho <sa78c45491>
2021-08-09 11:02:34 -0300

    Alterações para ser utilizadas com apenas um csv

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #fluffy-invention
 
-Arquivos rpy em script são emparelhados com csv em csvs e os textos são substituídos e salvos na pasta saída utilizando o mesmo nome do rpy original.
+-Arquivos rpy em script são emparelhados com csv em csvs e os textos são substituídos e salvos na pasta saída utilizando o mesmo nome do rpy original.
+-Observação, o CSV deve ser separado por ";" (Ponto e Vírgula), oposto a separado por ","(Vírgula) como devem estar o CSVs que nós temos.
--- translations.py
+++ translations.py
@@ -13,16 +13,22 @@ for f in listdir("scripts"):
 for f in listdir("csvs"):
 	csvs.append(str(f))
 
+cData = open("csvs/"+csvs[0], encoding="latin-1").readlines()
+#alteração para sempre pegar o csv na posição zero, para que se use apenas 1 csv. 
+
+idxScrpt = 1
+#variável a ser usada quando tiver sendo usado apenas um csv
+
 idx = 0
 for script in scripts:
 	saida = open("saida/"+script, "w")
 	#criação do arquivo de ´saída com o mesmo nome do arquivo de script
 
 	sData = open("scripts/"+script, encoding="latin-1").readlines()
-	cData = open("csvs/"+csvs[idx], encoding="latin-1").readlines()
+	#cData = open("csvs/"+csvs[idx], encoding="latin-1").readlines()
 	#o for itera todos os arquivos na pasta script, e então abro ele separando por linha, então faço o mesmo com o CSV na mesma posição
 
-	idxScrpt = 1
+	#idxScrpt = 1
 	for linha in sData:
 		linha = linha.split('"')
 
